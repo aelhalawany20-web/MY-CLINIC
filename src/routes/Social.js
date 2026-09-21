fd1eb13ae97d941e269f019b8c8e57098d8a2cf4
abdo helw <aelhalawany20@gmail.com>
2026-09-21 11:49:31 +0200

    first

--- src/routes/Social.js
+++ src/routes/Social.js
@@ -5,7 +5,7 @@ const Social = () => {
   return (
     <div className='social'>
       زوروا صفحتنا على الفيسبوك
-      <a href="https://www.facebook.com/profile.php?id=61565606768972" target="_blank">اضغط هنا</a>
+      <a href="https://www.facebook.com/profile.php?id=61565606768972" target="_blank" rel="noreferrer">اضغط هنا</a>
     </div>
   )
 }
