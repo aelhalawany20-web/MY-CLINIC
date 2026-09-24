9f072124c95882edbd4ff7ea1b533da964470685
abdo helw <aelhalawany20@gmail.com>
2026-09-24 12:08:45 +0200

    specializationLogo

--- src/routes/specialization/SpecialHome.js
+++ src/routes/specialization/SpecialHome.js
@@ -1,17 +1,80 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import kids from "./pictures/kids.jpg"
+import abdomen from "./pictures/abdomen.png"
+import heart from "./pictures/heart.jpg"
+import heartChildren from "./pictures/heartchildren.png"
+import nose from "./pictures/nose.png"
+import surgery from "./pictures/surgery.jpg"
+import surgeryChildren from "./pictures/surgerychildren.jpg"
 
 function SpecialHome() {
   return (
     <div className='specialHome'>
-      <Link to = "children">عيادة الأطفال</Link>
-      <Link to = "Emergencychildren">عيادة طوارئ أطفال</Link>
-      <Link to = "abdomen">عيادة الباطنة</Link>
-      <Link to = "heart">عيادة القلب</Link>
-      <Link to = "heartchildren">عيادة قلب - أطفال</Link>
-      <Link to = "nose">عيادة الأنف والأذن</Link>
-      <Link to = "surgery">عيادة الجراحة</Link>
-      <Link to = "surgerychildren">عيادة الجراحة - أطفال</Link>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={kids} alt="kids" />
+          </div>
+          <Link className='link' to="children">عيادة الأطفال</Link>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={kids} alt="kids" />
+          </div>
+          <Link className='link' to="Emergencychildren">عيادة طوارئ الأطفال</Link>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={abdomen} alt="kids" />
+          </div>
+          <Link className='link' to="abdomen">عيادة الباطنة</Link>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={heart} alt="kids" />
+          </div>
+          <Link className='link' to="heart">عيادة القلب</Link>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={heartChildren} alt="kids" />
+          </div>
+          <Link className='link' to="heartchildren">عيادة قلب - أطفال</Link>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={nose} alt="kids" />
+          </div>
+          <Link className='link' to="nose">عيادة الأنف والأذن</Link>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={surgery} alt="kids" />
+          </div>
+          <Link className='link' to="surgery">عيادة الجراحة العامة</Link>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='box'>
+          <div className='image'>
+            <img src={surgeryChildren} alt="kids" />
+          </div>
+          <Link className='link' to="surgerychildren">عيادة الجراحة - أطفال</Link>
+        </div>
+      </div>
       <Link to = "surgerywomen">عيادة الجراحة النسائية</Link>
       <Link to = "food">عيادة التغذية العلاجية</Link>
       <Link to = "bones">عيادة العظام</Link>
